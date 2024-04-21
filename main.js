2235614b86fb6b9ebf3ad648ffca029ba0d50388
Sky <136200445+1Ash@users.noreply.github.com>
2024-04-21 13:28:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 const axios = require('axios');
 
-
 async function getPrevNames(userId) {
     try {
-        const response = await axios.get(`http://api.elitebots.xyz/api/prevnames/${userId}`);
-        return response.data.prevNames
+        const response = await axios.get(`http://elitebots.xyz/api/prevnames/${userId}`);
+        return response.data.prevNames;
     } catch (error) {
-        console.error('Erreur lors de la récupération des noms précédents');
-        }
+        console.error('erreur lors de la récupération des noms précédents');
+        throw error
+    }
 }
 
 async function ping() {
     try {
-        const response = await axios.get(`http://api.elitebots.xyz/api/latence`);
-        return response.data.ping
-    } catch (error) {
-        console.error("Erreur lors de la récupération de la latence de l'api");
+        const response = await axios.get(`http://elitebots.xyz/api/latence`, { timeout: 3000 })
+        if (response && response.data && response.data.ping) {
+            return response.data.ping;
+        } else {
+            throw new Error("Réponse invalide de l'API");
         }
+    } catch (error) {
+        console.error("Erreur lors de la récupération de la latence de l'API:");
+        throw error;
+    }
 }
 
-console.log("La connexion à l'api Elite Bots a été établie avec succès.")
-module.exports = {getPrevNames, ping };
+console.log("La connexion à l'api Elite Bots a été établie avec succès.");
+
+module.exports = { getPrevNames, ping };
